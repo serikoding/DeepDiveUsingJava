293eebc43ce4b378ab323c595847a5798b782597
serikmgl@gmail.com <64537638+serikmgl@users.noreply.github.com>
2023-01-28 13:25:43 +0100

    My first coomitt

---
+++ src/main/java/BubbleSort/Main.java
@@ -0,0 +1,15 @@
+package BubbleSort;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] myarray = {65,89,47,51,23,26,45,48,67,21,35,69,21,5};
+
+        
+
+
+
+
+
+    }
+}
